52654786a6012c7aae567978e1f2f2b867852669
Brett Lee <brettlee@gmail.com>
2020-08-30 03:12:53 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-//importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js');
 
 self.addEventListener('install', event => {
     self.skipWaiting();   
